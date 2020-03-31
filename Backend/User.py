888cf78a61137236b5d9437b2a013d3e4ff5d035
Abigail Butka <butkaa@my.erau.edu>
2020-03-31 17:45:19 -0400

    Added Hours class/Updated User
    Pushed handling of time to Hours.py to simplify User.py. Additions work
    DeviantHours is a subclass of Hours, but its functions are no different yet and it is not being used, thus it is not its own class right now.

--- Backend/User.py
+++ Backend/User.py
@@ -1,34 +1,22 @@
 import numpy as np
+import Hours
 
 class User:
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.userHours = Hours.Hours()
+        self.userDeviantHours = Hours.Hours()
+        self.times = self.userHours.times
         # times needs to a numpy int array for the Schedule
-        self.times = np.zeros([7, 48], dtype=int)
 
     def changeAvailibility(self, day, time):
-        self.times[day][time] ^= 1
+        self.userHours.changeAvailibility(day, time)
 
     def save(self):
-        saveFile = open(self.name + ".csv", "w")
-        saveFile.write(self.name + "," + self.id + ",\n")
-        for day in self.times:
-            dayLine = ""
-            for time in day:
-                dayLine = dayLine + str(time) + ","
-            saveFile.write(dayLine + "\n")
-        saveFile.close()
+        self.userHours.save()
 
     def load(self, name):
-        loadFile = open(name + ".csv", "r")
-        info = loadFile.readline().split(",")
-        self.name = info[0]
-        self.id = info[1]
-        for day in range(int(self.times.size / self.times[0].size)):
-            dayTimes = loadFile.readline().split(",")
-            for times in range(self.times[day].size):
-                self.times[day][times] = dayTimes[times]
-
+        self.userHours.load(name)
 
 
