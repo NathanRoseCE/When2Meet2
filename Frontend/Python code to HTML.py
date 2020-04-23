200d9dd0616b411d4b524536c265fb291a45ff0e
Teja Tiriveedhi <tiriveec@my.erau.edu>
2020-04-23 16:20:05 -0400

    Python Code to HTML
    This is the python code that reads the csv file generated from the meeting Algorithm and converts it to a HTML webpage

---
+++ Frontend/Python code to HTML.py
@@ -0,0 +1,14 @@
+import pandas as pd 
+  
+# to read csv file named "samplee" 
+a = pd.read_csv("Test.csv") 
+  
+# to save as html file 
+# named as "Table" 
+a.to_html("Table.htm") 
+  
+# assign it to a  
+# variable (string) 
+html_file = a.to_html() 
+
+
